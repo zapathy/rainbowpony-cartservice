d9f7c5482df253c9536160a527eb4342dc13096b
zapathy <apathy.zsombor@gmail.com>
2019-04-24 10:54:53 +0200

    created cart item model

---
+++ src/main/java/com/rainbowpony/cartservice/model/CartItem.java
@@ -0,0 +1,25 @@
+package com.rainbowpony.cartservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    Integer amount;
+}
